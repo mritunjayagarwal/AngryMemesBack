af0a8a0f669826288add4bc8dce59be7ffcb10a5
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2024-07-23 01:09:07 +0530

    Minor

---
+++ src/components/profile.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { auth, db } from "./firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { Unity, useUnityContext } from "react-unity-webgl";
+
+function Profile() {
+  const [userDetails, setUserDetails] = useState(null);
+  const fetchUserData = async () => {
+    auth.onAuthStateChanged(async (user) => {
+      console.log(user);
+
+      const docRef = doc(db, "Users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setUserDetails(docSnap.data());
+        console.log(docSnap.data());
+      } else {
+        console.log("User is not logged in");
+      }
+    });
+  };
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  async function handleLogout() {
+    try {
+      await auth.signOut();
+      window.location.href = "/login";
+      console.log("User logged out successfully!");
+    } catch (error) {
+      console.error("Error logging out:", error.message);
+    }
+  }
+
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "build/build.loader.js",
+    dataUrl: "build/build.data",
+    frameworkUrl: "build/build.framework.js",
+    codeUrl: "build/build.wasm",
+  });
+
+  return <Unity unityProvider={unityProvider} style = {{position: "absolute", top: 0, left: 0, right: 0, bottom: 0}}/>;
+}
+export default Profile;
